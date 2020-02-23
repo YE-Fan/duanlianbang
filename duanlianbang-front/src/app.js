30cd554169100796796a1dfcc99074db3342993f
Mihile <azuretodd@foxmail.com>
2020-02-23 20:38:41 +0800

    添加： 前端添加dva-logger

--- duanlianbang-front/src/app.js
+++ duanlianbang-front/src/app.js
@@ -1,15 +1,14 @@
 import * as React from 'react'
 import dva, { connect } from 'remax-dva';
+import createLogger from 'dva-logger';
 import models from '@/models/models.js'
 import './app.css'
 
 const app = dva();
-
+app.use(createLogger());
 models.forEach(model => {
     app.model(model);
 });
-
-
 
 class App extends React.Component {
     render(){
